98104e17753b5953187b0058a501748a51887173
= <=>
2020-08-27 14:14:24 +0100

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+    ./hosts/melvin.nix
+    ./home/configuration.nix
+  ];
+
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console = {
+    font = lib.mkDefault "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+    keyMap = "uk";
+  };
+
+  time.timeZone = "Europe/London";
+
+  environment.systemPackages = with pkgs; [];
+
+  services.xserver = {
+    enable = true;
+    layout = "gb";
+    displayManager.lightdm.enable = true;
+    displayManager.defaultSession = "home-manager";
+    displayManager.session = [
+      {
+        name = "home-manager";
+        manage = "desktop";
+        start = ''
+          ${pkgs.runtimeShell} $HOME/.hm-xsession
+          waitPID=$!
+        '';
+      }
+    ];
+  };
+}
---
+++ hosts/melvin.nix
@@ -0,0 +1,55 @@
+{ config, lib, pkgs, ... }:
+
+{
+  boot = {
+    initrd.availableKernelModules = [
+      "ahci"
+      "ohci_pci"
+      "ehci_pci"
+      "pata_atiixp"
+      "xhci_pci"
+      "firewire_ohci"
+      "usbhid"
+      "usb_storage"
+      "sd_mod"
+    ];
+
+    initrd.kernelModules = [ ];
+    kernelModules = [ "kvm-amd" ];
+    extraModulePackages = [ ];
+
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      grub = {
+        enable = true;
+        devices = [ "nodev" ];
+        efiSupport = true;
+        useOSProber = true;
+      };
+    };
+  };
+
+  networking = {
+    hostName = "melvin";
+    useDHCP = false;
+    interfaces.enp3s0.useDHCP = true;
+  };
+  
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/9dd3e046-4e6b-48a3-9592-5f8b9c4cdf28";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/6F00-D804";
+      fsType = "vfat";
+    };
+  };
+
+  swapDevices = [ ];
+
+  nix.maxJobs = lib.mkDefault 8;
+
+  system.stateVersion = "20.03";
+}
